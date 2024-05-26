9060e22f2abde3b7a27aadec444f6af9656bdb1e
famechan <famechan@deidara.ru>
2024-05-26 12:45:35 +0900

    nu pochti gotovo

--- cities/main/urls.py
+++ cities/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('home', views.index, name='home'),
+    path('', views.index, name='home'),
 
     path('brest', views.brest, name='brest'),
     path('kerch', views.kerch, name='kerch'),
